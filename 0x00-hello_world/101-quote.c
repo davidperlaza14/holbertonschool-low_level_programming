960bf2868ef9a0ba01bccb5c9a11a0d8a99cef44
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-10 23:28:26 -0500

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, q, 59);
+	return (1);
+}
