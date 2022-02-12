625dcf8f0ab75b279b54fa67ba04c2bab5d75aec
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-11 19:46:42 -0500

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char apb;
+
+	for (apb = 'a'; apb <= 'z'; apb++)
+	{
+		putchar(apb);
+	}
+	putchar('\n');
+	return (0);
+}
