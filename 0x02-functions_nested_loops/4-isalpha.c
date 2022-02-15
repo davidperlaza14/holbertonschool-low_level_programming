a0fe2ea56789b605130e47e372b431c075553aa9
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-15 16:22:30 -0500

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * It is recommended we use the isalpha() function to check whether a character is an alphabet or not.
- * Returns 1 if c is a letter, lowercase or uppercase
- * Returns 0 otherwise
+ * _isalpha - rite a function that checks for alphabetic character.
+ * @c: identify between alphabetic characters
+ * It is recommended we use the isalpha()
+ * function to check whether a character is an alphabet or not.
+ * Return: 1 if c is a letter, lowercase or uppercase
+ *
  */
 
 int _isalpha(int c)
 
 {
 	if (((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <= 'z')))
-	{
-		return 1;
-	}
-
-         	return 0;
-
+		return (1);
+	return (0);
 }
