16ed1c13b52c4e07d4d59e5c13612a0651c6db9a
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-15 12:56:24 -0500

    0x02. C - Functions, nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	int ch;
 	for (ch='a'; ch<= 'z'; ch++)
-	  _putchar (ch);
-	   _putchar('\n');
+		_putchar (ch);
+	_putchar('\n');
 }
