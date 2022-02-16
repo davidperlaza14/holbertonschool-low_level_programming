28413c08adc7475a979122850f61c6f03a6e6d51
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-15 23:02:56 -0500

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Add two integers
+ * @n: First input in the add
+ * @g: Second input in the add
+ * Return: Add product
+ */
+
+int add(int d, int p)
+{
+	return (d + p);
+}
