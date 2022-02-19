e35504c89c96fdeb26173286962bb1a63c69862a
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-18 21:47:53 -0500

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,10 +9,17 @@
 
 void print_number(int n)
 {
-	int n;
 
-	putchar('n');
+    if (n < 0) {
+        putchar('-');
+        n = -n;
+    }
 
+    if (n == 0)
+        putchar('0');
 
-	return (0);
+    if (n/10)
+        printnumber(n/10);
+
+    putchar(n%10 + '0');
 }
