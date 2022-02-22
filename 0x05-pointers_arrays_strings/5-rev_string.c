1df3f2211b8b69713f75e85dbac60b2788590572
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-21 22:58:28 -0500

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse a string.
+ * @s: String input.
+ * Return: no return.
+ */
+
+void rev_string(char *s)
+{
+	char *cadena = s;
+	char t;
+
+	/*Fijar el puntero al final*/
+	for (; *(s); s++)
+	{
+	}
+	s--; /*no null*/
+
+	 /*Bucle para cambiar las letras*/
+	for (; s >= cadena; s--)
+	{
+		t = *cadena;
+		*cadena  = *s;
+		*s = t;
+		cadena++;
+	}
+}
