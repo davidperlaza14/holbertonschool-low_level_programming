32aa1977b3394dcf69aaa28270545e2e32653f10
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-21 21:33:42 -0500

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Print a string.
+ * @str: Input value.
+ * Return: no return.
+ */
+
+void _puts(char *str)
+{
+	int c;
+
+	for (; *str != ('\0'); c++)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
