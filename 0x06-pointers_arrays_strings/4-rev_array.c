19c9ce7a1b78c7a6eb9b9eb12d6ab4c009733946
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-24 21:09:53 -0500

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * reverse_array - Reverse the content of an array.
+ * @a: Input of the string.
+ * @n: Lengh of the string.
+ * Return: no return.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int*p = a; /*Apunta a la cadena*/
+	int c = 0; /*inicio*/
+	int temp; /* Posicion temporal*/
+
+	n--;/*no null*/
+	while (c < n)
+	{
+		temp = *(p + c);
+		*(p + c) = *(p + c);
+		*(p + n) = temp;
+		c++;
+		n--;/*decrementa la posicion*/
+	}
+}
