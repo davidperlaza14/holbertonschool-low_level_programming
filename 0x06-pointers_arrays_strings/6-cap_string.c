3911f6ad8193002c7d304c475f69ae99d6a3b62e
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-24 22:47:24 -0500

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * cap_string - Capitalize words of a string.
+ * @s: String to capitalize.
+ * Return: String capitalized.
+ */
+char *cap_string(char *s)
+{
+	char *str = s; /* Pointer to string in main */
+	char ar[] = "! \t\n,;.?\"(){}"; /* Array of special characters */
+	char *par = &ar[0]; /* Pointer to the array */
+
+	if (*str > 96 && *str < 123)
+		*str -= 32;
+
+	while (*str != '\0')
+	{
+		for (par = &ar[0]; *par != '\0'; par++)
+		{
+			if (*str == *par)
+			{
+				str++;
+				if (*str > 96 && *str < 123)
+					*str -= 32;
+				else
+					par = &ar[0];
+			}
+		}
+		str++;
+	}
+	return (s);
+}
