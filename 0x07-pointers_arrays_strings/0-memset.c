40244fa5e5b819c5866c89f2fbb5679de6684ff9
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-28 19:37:16 -0500

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _memset - Fill the first n bytes of the memory.
+ * @n: Number of bytes to fill.
+ * @s: Space in memory to fill.
+ * @b: Value to be set.
+ * Return: Pointer to the block of memory.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int c = 0;
+
+	while (c < n)
+	{
+		s[c] = b;
+		c++;
+	}
+
+	return (s);
+}
