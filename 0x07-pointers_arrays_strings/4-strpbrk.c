d3bb88c3836ef0d69c14a94e6ae6da46c3c306aa
davidperlaza14 <davidperlaza14@gmail.com>
2022-02-28 22:18:22 -0500

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - Locate the first occurrence in a string.
+ * @s: String to search.
+ * @accept: Character to be match.
+ * Return: Pointer since the first occurrence.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a = accept;
+
+	while (*s != '\0')
+	{
+		for (accept = a; *accept != '\0'; accept++)
+		{
+			if (*s == *accept)
+				return (s);
+		}
+		s++
+	}
+	return (NULL);
+}
