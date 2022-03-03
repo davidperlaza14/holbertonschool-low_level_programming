8308265bdbea07e8918222779c4cdb129c4a675c
davidperlaza14 <davidperlaza14@gmail.com>
2022-03-02 21:43:29 -0500

    2-strlen_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Print a string in reverse.
+ * @s: String to print.
+ * Return: no return.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s =='\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
