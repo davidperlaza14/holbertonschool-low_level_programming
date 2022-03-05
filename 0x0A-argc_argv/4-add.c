1018fe885d1ebc848ea9ee1a314e1b592e40f115
davidperlaza14 <davidperlaza14@gmail.com>
2022-03-05 13:41:54 -0500

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,10 +3,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * main - add 2 positive numbers and print the result
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Description: If no number is passed to program, print 0.
+ * If one of the numbers contain non-digits, print Error.
+ * Return: 1 if error, 0 if function runs properly.
+ */
+
+int main(int argc, char *argv[])
+{
+	int total, i;
+	char *p;
+	int num;
+
+	total = 0;
+	if (argc > 1)
+	{
+		for (i = 1; argv[i]; i++)
+		{
+			num = strtol(argv[i], &p, 10);
+			if (!*p)
+				total += num;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", total);
+	return (0);
+}
+/**
  * _argv - Evaluate *argv for special characters.
  * @s: string input.
  * Return: string evaluated.
- */
+
 int _argv(char *s)
 {
 	if (!isdigit(*s))
@@ -14,12 +47,12 @@ int _argv(char *s)
 	printf("Error\n");
 	return (1);
 }
-/**
+
  * main - Adds positive numbers.
  * @argc: Argument's counter.
  * @argv: Argument's strings.
  * Return: 0 for the add, 1 for error.
- */
+
 int main(int argc, char *argv[])
 {
 	int c, add = 0;
@@ -40,3 +73,4 @@ int main(int argc, char *argv[])
 		printf("%d\n", 0);
 	return (0);
 }
+*/
