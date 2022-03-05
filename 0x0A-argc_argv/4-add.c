e28e953e53c8da0013c53775a3e2704341956cb5
davidperlaza14 <davidperlaza14@gmail.com>
2022-03-04 22:59:39 -0500

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _argv - Evaluate *argv for special characters.
+ * @s: string input.
+ * Return: string evaluated.
+ */
+int _argv(char *s)
+{
+	if (!isdigit(*s))
+
+		return (*s++);
+
+	printf("Error\n");
+	return (1);
+}
+/**
+ * main - Adds positive numbers.
+ * @argc: Argument's counter.
+ * @argv: Argument's strings.
+ * Return: 0 for the add, 1 for error.
+ */
+int main(int argc, char *argv[])
+{
+	int c, add = 0;
+
+	if (argc > 1)
+	{
+		for (c = 1; c < argc; c++)
+		{
+			if (atoi(argv[c]))
+				add += atoi(argv[c]);
+			else if (!isdigit(*argv[c]))
+				return (_argv(*argv));
+			add += atoi(argv[c]);
+		}
+		printf("%d\n", add);
+	}
+	else
+		printf("%d\n", 0);
+	return (0);
+}
