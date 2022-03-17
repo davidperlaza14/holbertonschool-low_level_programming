818613cc56d6a5d5de176ebc007a6e187509cf33
davidperlaza14 <davidperlaza14@gmail.com>
2022-03-17 14:39:08 -0500

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#infndef DOG_H
+#ifndef DOG_H
 #define DOG_H
 /**
  * struct dog - ID for dogs.
@@ -14,10 +14,10 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struc dog *d);
+void print_dog(struct dog *d);
 typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, achar *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 int slen(char *s);
 void free_dog(dog_t *d);
 
-#endif /*DOG_H*/
+#endif /* DOG_H */
