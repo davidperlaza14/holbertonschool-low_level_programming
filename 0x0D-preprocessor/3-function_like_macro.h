ff25199aa9a69af873ae4ba83bb62178c7209698
davidperlaza14 <davidperlaza14@gmail.com>
2022-03-16 21:50:53 -0500

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _3FUNCTION_LIKE_MACRO_
+#ifndef _3_FUNCTION_LIKE_MACRO_
 #define _3_FUNCTION_LIKE_MACRO_
 
 #define ABS(x) ((x) < (0) ? ((x) * (-1)) : x)
 
-#endif /* _3_FUNCTION_LIKE_MACRO_
+#endif /* _3_FUNCTION_LIKE_MACRO_ */
