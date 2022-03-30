38743dc1018bd582424d704a719d40225ae3bf43
davidperlaza14 <davidperlaza14@gmail.com>
2022-03-29 20:56:30 -0500

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: memory location
+ */
+void free_list(list_t *head)
+{
+	list_t *b;
+
+	while(head != NULL)
+	{
+		b = head;
+		head = head->next;
+		free(b ->str);
+		free(head);
+	}
+
+}
