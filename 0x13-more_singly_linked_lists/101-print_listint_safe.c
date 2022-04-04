6d074346861603066be295d59435ed1ef4177cae
davidperlaza14 <davidperlaza14@gmail.com>
2022-04-04 17:27:06 -0500

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * print_listint_safe - Print a single and circular linked list.
+ * @head: Points to the first node of the linked list.
+ * Return: Number of nodes of the linked list, 98 on failure.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t num_nodes = 0;
+	const listint_t *new_head = NULL, *tail = NULL;
+
+	new_head = head;
+	while (new_head)
+	{
+		num_node++;
+		tail = new_head;
+		printf("[%p] %d\n",(void *)new_head, newheade->n);
+		new_head = new_head->next;
+
+		if (tail < new_head)
+		{
+			printf("-> [%p] %d\n", (void *)new_head, new_head->n);
+			break;
+		}
+	}
+	if (tail == tail->next)
+		printf("-> [%p] %d\n", (void *)new_head, new_head->n);
+
+	return (num_nodes);
+}
