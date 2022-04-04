7bd481f519d5472d4c5980e3108f01ea34cb8472
davidperlaza14 <davidperlaza14@gmail.com>
2022-04-04 14:57:37 -0500

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,8 +27,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				return (NULL);
 			}
 		}
-		new_node-> = tmp_node->next;
-		tmp_node->next new_node;
+		new_node->next = tmp_node->next;
+		tmp_node->next = new_node;
 	}
 	else
 	{
