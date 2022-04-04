192d73b8d5e41f8fba046f3f5a3188f1dcba981d
davidperlaza14 <davidperlaza14@gmail.com>
2022-04-04 14:37:18 -0500

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - Free a likend list.
+ * @head: Points to the head of the linked list.
+ * Return: No return.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp_pointer = NULL;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp_pointer = (*head)->next;
+		free(*head);
+		*head = tmp_pointer;
+	}
+}
