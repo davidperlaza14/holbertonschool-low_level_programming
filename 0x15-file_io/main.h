94bf171e62d67734677d089b3a85660dd1d64084
davidperlaza14 <davidperlaza14@gmail.com>
2022-04-08 19:24:06 -0500

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,16 @@
 #ifndef MAIN_H
-#define Main_H
+#define MAIN_H
 
 #include <stdio.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
 
-#endif/*MAIN_H*/
+#endif /* MAIN_H */
