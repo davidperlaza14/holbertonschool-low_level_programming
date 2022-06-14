bf6937ba7e4da7f5f0f5cdb1d08b725d7c94be4d
davidperlaza14 <davidperlaza14@gmail.com>
2022-06-14 13:41:28 -0500

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,36 +1,53 @@
 #include "hash_tables.h"
+
 /**
- *hash_table_print - prints a hash table in the order they appear
- *in the hash table array
- *@ht: pointer to the hash table
- *
- *Return: void
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the hash_node_t list to print
+ */
+void print_list(hash_node_t *h)
+{
+	while (h)
+	{
+		printf("'%s': '%s'", h->key, h->value);
+		if (h->next)
+			printf(", ");
+		h = h->next;
+	}
+}
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int iterator = 0;
-	hash_node_t *node;
-	int multiplenodes = 0;
+	unsigned long int i;
+	hash_node_t *node = NULL;
+	char *last_key = NULL;
+	unsigned long int index;
 
-	if (ht == NULL)
+	if (!ht)
 		return;
 
-	printf("{");
-	while (iterator < ht->size)
+	for (i = 0; i < ht->size; i++)
 	{
-		node = ht->array[iterator];
-		while (node != NULL)
-		{
-			if (multiplenodes == 0)
-			{
-				printf("'%s': '%s'", node->key, node->value);
-				multiplenodes = 1;
-			}
-			else
-				printf(", '%s': '%s'", node->key, node->value);
-			node = node->next;
-		}
-		iterator++;
+		if (ht->array[i] != NULL)
+			node = ht->array[i];
 	}
+
+	printf("{");
+
+	if (node)
+	{
+		last_key = node->key;
+		index = key_index((const unsigned char *)last_key, ht->size);
+		for (i = 0; i < ht->size; i++)
+		{
+			print_list(ht->array[i]);
+			if (ht->array[i] && i < index)
+				printf(", ");
+		}
+	}
+
 	printf("}\n");
 }
