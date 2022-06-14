3137dec0229d3b2eb7d63fdd762ce5235364cf28
davidperlaza14 <davidperlaza14@gmail.com>
2022-06-14 13:47:02 -0500

    3-4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,48 +1,27 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: is the hash table you want to add or update the key/value to
- * @key: is the key. key can not be an empty string
- * @value: is the value associated with the key.
- * Return: Returns: 1 if it succeeded, 0 otherwise
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: is the hash table you want to look into
+ * @key: is the key you are looking for
+ * Return: the value associated with the element
+ * or NULL if key couldn’t be found
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	hash_node_t *temp;
-	hash_node_t *hsnode;
+	hash_node_t *node;
 
 	if (ht == NULL)
-		return (0);
-	if (key == NULL || key[0] == '\0')
-		return (0);
-	if (ht->size == 0 || ht->array == NULL)
-		return (0);
+		return (NULL);
+	if (key[0] == '\0' || key == NULL)
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
-	temp = ht->array[index];
-	while (temp != NULL)
+	node = ht->array[index];
+	while (node != NULL)
 	{
-		if (strcmp(temp->key, key) == 0)
-		{
-			free(temp->value);
-			temp->value = strdup(value);
-			return (1);
-		}
-
-		temp = temp->next;
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
 	}
-	hsnode = malloc(sizeof(hash_node_t));
-	if (hsnode == NULL)
-		return (0);
-	hsnode->key = strdup(key);
-	hsnode->value = strdup(value);
-	if (ht->array[index] != NULL)
-	{
-		hsnode->next = ht->array[index];
-	}
-	else
-		hsnode->next = NULL;
-	ht->array[index] = hsnode;
-	return (1);
+	return (NULL);
 }
